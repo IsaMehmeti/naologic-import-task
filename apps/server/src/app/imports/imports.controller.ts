ef70f9c96a51022fe07f819c173fe3c357f970c1
Isa Mehmeti <isa-mehmeti@hotmail.com>
2024-08-01 13:29:34 +0200

    Support csv and excel files

--- apps/server/src/app/imports/imports.controller.ts
+++ apps/server/src/app/imports/imports.controller.ts
@@ -1,26 +1,48 @@
-import { Body, Controller, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ImportsService } from './imports.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Multer } from 'multer';
 import { S3Service } from '../aws/s3.service';
 import { ImportTypeValidationPipe } from './validation/import-type.validation.pipe';
 import { ImportType } from './enums/import-type.enum';
+import { AccountImportService } from './services/account-import.service';
 
 @Controller('imports')
 export class ImportsController {
     constructor(
         private readonly importsService: ImportsService,
-        private readonly s3Service: S3Service
+        private readonly s3Service: S3Service,
+        private readonly accountImportService: AccountImportService,
     ) { }
 
     @Post('')
     @UseInterceptors(FileInterceptor('file'))
-    async import(@UploadedFile() file: Express.Multer.File,
+    async import(
+        @UploadedFile(new ParseFilePipe({
+            validators: [
+                new MaxFileSizeValidator({ maxSize: 50 * 1024 * 1024 }), // <-- 50MB for now
+            ],
+        }),
+        ) file: Express.Multer.File,
         @Body('type', ImportTypeValidationPipe) type: ImportType,
     ) {
-        const result: any = await this.importsService.parseCSV(file.buffer);
+        // <-- check file excel or csv file type
+        const fileType = this.importsService.getFileType(file.originalname)
+        let result: any[];
+        if (fileType === 'csv') {
+            result = (await this.importsService.parseCSV(file.buffer)).data;
+        } else {
+            result = this.importsService.parseExcel(file.buffer);
+        }
 
+        // <-- get import service type based on type body param
+        const typeImportService = this[`${type}ImportService`];
 
-        await this.s3Service.uploadObject(file.buffer, file.originalname, 'naologic-task-isa');
+        const mappedData = typeImportService.mapData(result);
+
+        return mappedData[0];
+        // const transformedDta = typeImportService.transFormData(mappedData);
+
+        // await this.s3Service.uploadObject(file.buffer, file.originalname, 'naologic-task-isa');
     }
 }
--- apps/server/src/app/imports/imports.service.ts
+++ apps/server/src/app/imports/imports.service.ts
@@ -1,18 +1,17 @@
-import { Injectable } from "@nestjs/common";
-import * as Papa from 'papaparse'
-import * as stream from 'stream'
-
+import { Injectable, UnsupportedMediaTypeException } from "@nestjs/common";
+import * as Papa from 'papaparse';
+import * as stream from 'stream';
+import * as XLSX from 'xlsx';
+import * as path from 'path';
 @Injectable()
 export class ImportsService {
-
-    async parseCSV(file: Buffer) {
+    async parseCSV(file: Buffer): Promise<Papa.ParseResult<any>> {
         return new Promise((resolve, reject) => {
             const bufferStream = new stream.PassThrough();
             bufferStream.end(file);
             Papa.parse(bufferStream, {
                 header: true,
-                complete: function (results) {
-                    console.log(results, 'results');
+                complete: function (results: Papa.ParseResult<any>) {
                     resolve(results);
                 },
                 error: function (error) {
@@ -20,6 +19,25 @@ export class ImportsService {
                 }
             });
         });
+    }
 
+    parseExcel(buffer: Buffer): any[] {
+        const workbook = XLSX.read(buffer, { type: 'buffer' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        return XLSX.utils.sheet_to_json(sheet);
+    }
+
+    getFileType(filename: string): string {
+        const ext = path.extname(filename).toLowerCase();
+        switch (ext) {
+            case '.csv':
+                return 'csv';
+            case '.xls':
+            case '.xlsx':
+                return 'excel';
+            default:
+                throw new UnsupportedMediaTypeException('Unsupported file type');
+        }
     }
 }
